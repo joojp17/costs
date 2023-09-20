43aac0a4297a537150cf1e383b3cb0b3dff6f90d
João Pedro <contatojptm@gmail.com>
2023-09-19 22:48:10 -0300

    first commit

---
+++ src/components/form/Select.js
@@ -0,0 +1,24 @@
+import styles from './css/Select.module.css'
+
+function Select({ text, name, options, handlerOnChange, value }) {
+  return (
+    <div className={styles.form_control}>
+      <label htmlFor={name}>{text}:</label>
+      <select
+        name={name}
+        id={name}
+        onChange={handlerOnChange}
+        value={value || ''}
+      >
+        <option>Selecione uma opção</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+export default Select
